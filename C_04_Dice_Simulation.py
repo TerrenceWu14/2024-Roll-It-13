36dd2c6ea4594203303614354b7a75b6dc573185
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2024-01-31 20:39:50 +1300

    Added a way to roll 2 dice
    Also added a way to let the user know if they managed to get a double score opportunity or not. (item in range is for testing purposes only)

--- C_04_Dice_Simulation.py
+++ C_04_Dice_Simulation.py
@@ -1,5 +1,29 @@
 import random
 
+
+def roll_die():
+    result = random.randint(1, 6)
+    return result
+
+
+# Main routine goes here
 for item in range (0, 10):
-    dice_roll = random.randint(1, 3)
-    print(dice_roll, end="\t")
+    user_score = 0
+    double_score = "no"
+
+    # Rolls two dice (First rolls)
+    roll_1 = roll_die()
+    print(roll_1)
+    roll_2 = roll_die()
+    print(roll_2)
+    # Checks if it's possible to double the score
+
+    if roll_1 == roll_2:
+        double_score = "yes"
+
+    # Find the total points after the first dice roll
+    user_points = roll_1 + roll_2
+
+    # Shows the result of the dice rolls
+    print(f"Die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+    print(f"Double score opportunity: {double_score}")
